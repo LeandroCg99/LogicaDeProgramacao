abf43171d2810343353fbd655c277bda03d4dca8
LeandroCg99 <leandro.cgeronimo@hotmail.com>
2023-01-24 19:39:17 -0300

    Aula 21 e 22

---
+++ LogicaProg/src/LogicaProg21e22.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/*
+ exercício: Cadastro de login
+ Eu, como administrador do sistema, gostaria de permitir
+ que os próprios usuários cadastrem o nome usuário (login)
+ no sistema para agilizar o processo de migração do sistema.
+ 
+ O usuário não pode deixar o login null "", ou criar um usuário
+ com login "admin ou "administrador". Se o valor inserido 
+ for válido, o sistema deverá exibir uma mensagem: 
+ "<nome do usuário> cadastrado com sucesso"
+ Senão, o sistema deverá mostrar o erro "Usuário inválido".
+ */
+public class LogicaProg21e22 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Digite o seu nome: ");
+		String nome = input.next();
+		
+		if(nome.equals("") || nome.equalsIgnoreCase("admin") || nome.equalsIgnoreCase("administrador")) {
+			System.out.println("Usuario invalido");
+			
+		}else {
+			System.out.println(nome + " cadastrado com sucesso");
+		}
+		
+	}
+	
+}
