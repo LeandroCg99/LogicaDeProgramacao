cfb5a58d71f349276261255c289dede4a1c26492
LeandroCg99 <leandro.cgeronimo@hotmail.com>
2023-01-30 23:48:45 -0300

    Aula 26

---
+++ LogicaProg/src/LogicaProg26.java
@@ -0,0 +1,33 @@
+
+public class LogicaProg26 {
+	public static void main(String[] args) {
+
+		int dia = 7;
+		switch (dia) {
+
+		case 1:
+			System.out.println("Segunda");
+			break;
+		case 2:
+			System.out.println("Terca");
+			break;
+		case 3:
+			System.out.println("Quarta");
+			break;
+		case 4:
+			System.out.println("Quinto");
+			break;
+		case 5:
+			System.out.println("Sexta");
+			break;
+		case 6:
+			System.out.println("Sabado");
+			break;
+		case 7:
+			System.out.println("Domingo");
+			break;
+
+		}
+		System.out.println("Fim do programa!");
+	}
+}
