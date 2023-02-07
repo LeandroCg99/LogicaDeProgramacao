bf9250f3cdc2f896bc1dd8599a0a59b4c48d05cd
LeandroCg99 <leandro.cgeronimo@hotmail.com>
2023-02-06 23:16:33 -0300

    Aula 28

---
+++ LogicaProg/src/LogicaProg28.java
@@ -0,0 +1,12 @@
+
+public class LogicaProg28 {
+	public static void main(String[] args) {
+
+		int contador = 0;
+		while (contador <= 10) {
+
+			System.out.println("Contador: " + contador);
+			contador = contador + 1;
+		}
+	}
+}
